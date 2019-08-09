2029c8796a03ae00b2611fe04cdf91d5f9243c79
16kbf@queensu.ca <33009041+kaiferrall@users.noreply.github.com>
2019-08-09 15:15:27 -0400

    documents routes file.

--- app/api/routes/user.js
+++ app/api/routes/user.js
@@ -4,17 +4,35 @@ const authenticate = require("../auth/jwtAuth");
 const userAuthController = require("../controllers/user/userAuthController");
 
 Router.get("/test", userAuthController.test);
-
+/**
+@param -  json web token with containing user payload
+@return - new json web token or error if failed
+*/
 Router.get("/get", authenticate, userAuthController.getAuthenticatedUser);
-
+/**
+@param -  email, name, password, confirm
+@return - new json web token, user, and authentication status or error if failed
+*/
 Router.post("/create", userAuthController.create);
-
+/**
+@param -  email, confirm
+@return - new json web token, user, and authentication status or error if failed
+*/
 Router.post("/authenticate", userAuthController.authenticate);
-
+/**
+@param -  json web token containing email and name
+@return - success status if user found else returns error
+*/
 Router.put("/confirm-email", userAuthController.confirmEmail);
-
+/**
+@param -  email
+@return - success status if user found and sends email with link else returns error
+*/
 Router.post("/recover-password", userAuthController.sendPasswordLink);
-
+/**
+@param -  password, confirm
+@return - success status if valid passwords else returns error
+*/
 Router.put("/reset-password", userAuthController.resetPassword);
 
 module.exports = Router;
--- app/server.js
+++ app/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const keys = require("./config/keys");
